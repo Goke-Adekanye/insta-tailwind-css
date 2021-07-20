26e40849ab20f6c312b0fe714414028ce80e2de0
Goke Adekanye <adekanyeadegoke96@gmail.com>
2021-07-20 07:45:20 -0700

    added get-photos hooks

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,9 +1,18 @@
-import React from "react";
+import Skeleton from "react-loading-skeleton";
+import usePhotos from "../hooks/use-photos";
 
 export default function Timeline() {
+  const { photos } = usePhotos();
+  console.log(photos);
   return (
     <div className="container col-span-2">
-      <p>TIMELINE</p>
+      {!photos ? (
+        <Skeleton count={4} width={640} height={500} className="mb-5" />
+      ) : photos?.length > 0 ? (
+        photos.map((content) => <p>this is the {content.username}</p>)
+      ) : (
+        <p>Follow people to see photos</p>
+      )}
     </div>
   );
 }
